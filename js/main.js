c3601f966c21e2077caa2d2b30da0693e7e0d2ef
unurled <lch0ssah.dev@gmail.com>
2022-09-22 14:38:43 +0200

    Menu principal mobile

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ function logo(x) {
     const nav = document.getElementById("nav");
     if (nav.style.visibility === "visible") {
         nav.style.visibility = "collapse";
+        nav.style.display = "none";
         nav.style.opacity = "0";
     } else {
         nav.style.visibility = "visible";
+        nav.style.display = "block";
         nav.style.opacity = "1";
     }
 }
